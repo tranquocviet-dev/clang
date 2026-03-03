d983d516383746fb101353296bbf170e7cbb2a90
trquocviet-dev <trquocviet2k7@gmail.com>
2026-03-03 15:21:29 +0700

    adjust the format

--- less/sd.c
+++ less/sd.c
@@ -5,19 +5,19 @@
 #include <sys/dir.h>
 int main(void)
 {
-   struct dirent **namelist;
-   int n;
-   int i=0;
-   n = scandir(".", &namelist,NULL,alphasort);
-   if (n < 0)
-      perror("scandir");
-   else {
-      while (i<n) {
-         printf("%s\n", namelist[i]->d_name);
-         free(namelist[i]);
-         ++i;
-      }
-      free(namelist);
-   }
-   return 0;
+    struct dirent **namelist;
+    int n;
+    int i=0;
+    n = scandir(".", &namelist,NULL,alphasort);
+    if (n < 0)
+       perror("scandir");
+    else {
+       while (i<n) {
+          printf("%s\n", namelist[i]->d_name);
+          free(namelist[i]);
+          ++i;
+       }
+       free(namelist);
+    }
+    return 0;
 }
