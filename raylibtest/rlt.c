882144a0bc3bb495fc1b358d127e15452eaa9296
trquocviet-dev <trquocviet2k7@gmail.com>
2026-03-04 22:49:15 +0700

    raylibtest add

--- raylibtest/rlt.c
+++ raylibtest/rlt.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <raylib.h>
+#include <unistd.h>
+
+#define ScreenWidth 900
+#define ScreenHeight 600
 
 int main ()
 {
-    printf("well hello world");
+    Vector2 Dot1start =  {300.0f, 100.0f};
+    Vector2 Dot1end =  {300.0f, 150.0f};
+    Vector2 Dot2start =  {400.0f, 100.0f};
+    Vector2 Dot2end =  {400.0f, 150.0f};
+    Vector2 Dot3start =  {500.0f, 100.0f};
+    Vector2 Dot3end =  {500.0f, 150.0f};
+
+    InitWindow(ScreenWidth, ScreenHeight, "test");
+    SetTargetFPS(10);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+        EndDrawing();
+        WaitTime(1);
+        BeginDrawing();
+            DrawLineEx(Dot1start, Dot1end, 50, LIGHTGRAY);
+        EndDrawing();
+        WaitTime(1);
+        BeginDrawing();
+            DrawLineEx(Dot2start, Dot2end, 50, LIGHTGRAY);
+        EndDrawing();
+        WaitTime(1);
+        BeginDrawing();
+            DrawLineEx(Dot3start, Dot3end, 50, LIGHTGRAY);
+        EndDrawing();
+        WaitTime(1);
+    }
+    CloseWindow();
+
     return 0;
 }
